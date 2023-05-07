da5acf290d8b26f6ee3253bd5b6bb004f8b18831
astroud13 <113217155+astroud13@users.noreply.github.com>
2023-05-06 21:25:00 -0500

    Add files via upload

--- StroudAustinFinalProject.py
+++ StroudAustinFinalProject.py
@@ -1,22 +1,23 @@
 """
 Author:  Austin Stroud
-Date written: 04/27/2023
+Date written: 05/6/2023
 Assignment:   Module 8 Final Project
 Short Desc: A GUI program takes input from the user and tracks
 prizes selected for participation in a library reading program. 
 """
-#import tkinter 
-
+#import tkinter, imaging library, webbrowser, and messagebox
 from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox 
+import webbrowser
+
 
 #Creates a tkinter window object and sets its size and title
-
 reading = Tk()
-reading.geometry("600x500")
+reading.geometry("700x500")
 reading.title("Summer reading prize app")
 
 #Creates labels and entry boxes to collect user information 
-
 name_label = Label(reading, text="What is your name? ")
 name_label.grid(row=0, column=0)
 
@@ -43,8 +44,10 @@ email_entry.grid(row=3, column=1)
 
 # Creates a list of possible prizes and a listbox to display them
 my_reading_list = ["ink pen", "magnet", "canvas bag", "button", "bookmark"]
+my_reading_list.insert(0, "select a participation prize")
+print(my_reading_list)
 
-reading_list = Listbox(reading, selectmode=MULTIPLE, bg="yellow", fg="red")
+reading_list = Listbox(reading, bg="yellow", fg="red")
 reading_list.grid(row=4, column=1)
 
 #Populates the listbox with the possible prizes
@@ -91,15 +94,45 @@ check_button.grid(row=6, column=0)
 
 #Defines a function to remove selected prizes from the listbox
 def deleteme():
-    reading_list.delete(0,5)
+    reading_list.delete(0,1)
 
 del_button = Button(reading, text="Delete Prize", command=deleteme)
 del_button.grid(row=7, column=0)
 
+#Option menu to select a grand prize drawing
 drawingprizes = StringVar()
 drawingprizes.set("Choose a grand prize drawing.")
 
 drawingprizes = OptionMenu(reading, drawingprizes, "Indians Tickets", "Colts Tickets", "State Park Pass", "Museum Pass", "State Fair Pass", "Gas Card")
 drawingprizes.grid(row=8, column=0)
-reading.mainloop()
 
+#Code for the images in the application 
+reading_pic = ImageTk.PhotoImage(Image.open("books2.jpg"))
+pic_lbl = Label(reading, image=reading_pic, text="books")
+pic_lbl.grid(row=1, column=6)
+
+reading_pic2 = ImageTk.PhotoImage(Image.open("ribbon.png"))
+pic_lbl = Label(reading, image=reading_pic2, text="ribbon")
+pic_lbl.grid(row=8, column=1)
+
+# Defines a function to access the GitHub Repository in a web browser window
+def open_github():
+    webbrowser.open_new('repositorylink.html')
+
+button = Button(reading, text="Open GitHub Repository", command=open_github)
+button.grid(row=9, column=0)
+
+
+#Defines a function to exit the application 
+def exit():
+    answer = messagebox.askyesno("Hi", "Are you sure you want to exit? ")
+    if answer == 1:
+        reading.destroy()
+    else:
+        return
+
+exit_button = Button(reading, text="Exit", command=exit)
+exit_button.grid(row=4, column=6)
+
+#Start the main event loop
+reading.mainloop()
